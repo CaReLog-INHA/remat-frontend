1907b843e7e679dae0fe933ed033947692de67c9
JangMinhui <jangminhui1010@naver.com>
2026-05-10 03:11:53 +0900

    feat: 로그인 및 회원가입 UI 디자인 통일 및 페이지 전환 구현

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <LoginPage v-if="currentPage === 'login'" @change-page="goToPage" />
+  <SignupPage v-else-if="currentPage === 'signup'" @change-page="goToPage" />
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import LoginPage from './login.vue'
+import SignupPage from './sign_up.vue'
+
+// 현재 화면 상태 관리
+const currentPage = ref('login')
+
+// 화면 전환 함수
+const goToPage = (pageName) => {
+  currentPage.value = pageName
+}
+</script>
